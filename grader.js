20f2d09452264d3f3d1b6dcfc5dde6ea60e0b08a
Valery Yelanin <vyelanin@gmail.com>
2013-08-21 13:59:38 +0300

    grader.js added url support

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLFILE_DEFAULT = "http://still-lake-5601.herokuapp.com/index.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,20 +57,39 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkUrl = function(url, checkfile) {
+    rest.get(url).on('complete',function(data) {
+    	$ = cheerio.load(data);
+	var checks = loadChecks(checkfile).sort();
+	var out = {};
+	for (var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+	}
+	console.log(out);
+    });
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'URL to index.html', URLFILE_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+
+    var downURL2FileAndCheck = checkUrl(program.url, program.checks);
+    var outJsonUrl = JSON.stringify(downURL2FileAndCheck, null, 4);
+    console.log(outJsonUrl);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
